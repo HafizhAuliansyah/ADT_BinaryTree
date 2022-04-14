39efdaad01e1582e4382c778f31a01dfea07cfa2
M Hafizh Auliansyah <hafizh.auliansyah@gmail.com>
2022-04-14 09:50:18 +0700

    Melengkapi komentar header

--- BinaryTree.h
+++ BinaryTree.h
@@ -26,18 +26,32 @@ struct bTree{
 /* Inisialisasi root tree menjadi NULL */
 void bCreate(bTree *tree);
 
+/* Alokasi Node */
+/* Mengembalikan address node baru yang sudah ter-Alokasi */
 nAddress bCreateNode(infotype x);
 
+/* Insert Node */
+/* Menambahkan node baru sesuai parent yang dituju */
 void bInsert(bTree *root, nAddress parent, infotype x);
 
+/* Seach Node */
+/* Mengembalikan address node yang dicari */
 nAddress bSearch(bTree root, infotype src);
 
+/* Upgrade Node */
+/* Upgrade Parent dari node tertentu */
 void bUpgrade(bTree *root);
 
+/* Delete Node */
+/* Menghapus node tertentu */
 void bDelete(bTree *tree, nAddress *nodeDel);
 
+/* Depth Tree */
+/* Mengembalikan Depth dari tree */
 int bDepth(nAddress root);
 
+/* PRINT TREE */
+/* Menampilkan isi tree */
 void bPrint(nAddress, char tab[]);
 
 #endif
